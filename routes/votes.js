3f1f14fcd0f931fa0ea3444455ee6fd7230287fc
Suhas-C-V <cvsuhas.299@gmail.com>
2021-03-18 19:46:58 +0530

    comment votes

--- routes/votes.js
+++ routes/votes.js
@@ -15,14 +15,14 @@ router.post('/posts',(req,res)=>{
             res.status(500).json(err);
             throw err;
           }else{
-            db.query(`SELECT pv.user_id,pv.post_id,p.votes FROM post_votes pv,posts p WHERE pv.post_id = p.post_id and vote_id = ${data.insertId}`,(err,data)=>{
+            db.query(`SELECT pv.post_id,p.votes FROM post_votes pv,posts p WHERE pv.post_id = p.post_id and vote_id = ${data.insertId}`,(err,data)=>{
                 if(err) throw err;
                 res.status(201).json(data[0]);
             });
           }
       });
       } else {
-        db.query(`SELECT pv.user_id,pv.post_id,p.votes FROM post_votes pv,posts p WHERE pv.post_id = p.post_id and pv.post_id = ${post_id}`,(err,data)=>{
+        db.query(`SELECT pv.post_id,p.votes FROM post_votes pv,posts p WHERE pv.post_id = p.post_id and pv.post_id = ${post_id}`,(err,data)=>{
           if(err) throw err;
           res.status(201).json(data[0]);
       });
@@ -30,5 +30,30 @@ router.post('/posts',(req,res)=>{
   });
 });
 
+router.post('/comments',(req,res)=>{
+  var user_id = parseInt(req.body.user_id);
+  let com_id = parseInt(req.body.com_id);
+  db.query(`SELECT * FROM comment_votes WHERE com_id = ${com_id} and user_id = ${user_id}`,(err,data)=>{
+      if(err) throw err;
+      else if(data.length === 0){
+        db.query(`INSERT INTO comment_votes (user_id,com_id) VALUES (${user_id},${com_id});`,(err,data)=>{
+          if(err){
+            res.status(500).json(err);
+            throw err;
+          }else{
+            db.query(`SELECT cv.com_id,c.votes FROM comment_votes cv,comments c WHERE cv.com_id = c.com_id and v_id = ${data.insertId}`,(err,data)=>{
+                if(err) throw err;
+                res.status(201).json(data[0]);
+            });
+          }
+      });
+      } else {
+        db.query(`SELECT cv.com_id,c.votes FROM comment_votes cv,comments c WHERE cv.com_id = c.com_id and cv.com_id = ${com_id}`,(err,data)=>{
+          if(err) throw err;
+          res.status(201).json(data[0]);
+      });
+      }  
+  });
+});
 
 module.exports = router;
